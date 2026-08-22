3abc7c8f857c1785c19dcd3c6a45c7e8fb150085
RAVI SHANKAR <ravinits071@gmail.com>
2026-08-22 11:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (82.38%) - LeetSync

--- 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -3,7 +3,7 @@ public:
     int maxAscendingSum(vector<int>& nums) {
         int maxSum = nums[0];
         int currSum = nums[0];
-        int prevMaxSum = 0;
+       
 
         for(int i = 1; i < nums.size(); i++){
             if(nums[i] > nums[i-1]){
@@ -12,14 +12,11 @@ public:
             }
             else{
                 currSum = nums[i];
-                if(maxSum > prevMaxSum){
-                    prevMaxSum = maxSum;
-                }
-                maxSum = nums[i];
+               
                 
             }
         }
 
-        return max(maxSum,prevMaxSum);
+        return maxSum;
     }
 };
